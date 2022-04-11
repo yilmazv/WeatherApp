b6f7ba96aca2dfd1f9ff765e71b4183ba2878a2e
Vehbi Yilmaz <yilmazvtc@gmail.com>
2022-04-10 18:06:05 -0700

    Nearly finished code just need to add event listener to the search icon

---
+++ script.js
@@ -0,0 +1,28 @@
+let weather = {
+  apikey: "a1f3027a809cd30d530f8f313f26ee6c",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apikey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerHTML = "Weather in " + name + "!";
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+  },
+};
